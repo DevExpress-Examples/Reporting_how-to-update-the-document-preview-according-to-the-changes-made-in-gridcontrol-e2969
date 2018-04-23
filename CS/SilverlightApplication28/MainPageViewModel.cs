67efe5bedaeb43869383b790007070c706e1475d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication28/MainPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿// ...
+
+namespace SilverlightApplication28 {
+    public class MainPageViewModel {
+        public Person[] Persons { get; set; }
+
+        public MainPageViewModel() {
+            Persons = new[] { 
+                new Person("John", 25),
+                new Person("Mary", 33)
+            };
+        }
+    }
+}
---
+++ CS/SilverlightApplication28/Person.cs
@@ -0,0 +1,15 @@
+﻿// ...
+
+namespace SilverlightApplication28 {
+    public class Person {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age) {
+            Name = name;
+            Age = age;
+        }
+    }
+}
+
+
