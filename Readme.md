a9ddb858f55633140fab4c7be97ce877b10b1073
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to update the Document Preview according to the changes made in GridControl
+
+
+<p>This sample demonstrates how you can create a report document from a GridControl, and update its Document Preview on the same page, by clicking a button.</p>
+
+<br/>
+
+
---
+++ VB/SilverlightApplication28/MainPage.xaml.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.Xpf.Grid
+
+Namespace SilverlightApplication28
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+			Dim link As New VisualDataNodeLink(CType(gridControl1.View, TableView))
+			Dim model As New LinkPreviewModel(link)
+			documentPreview1.Model = model
+			link.CreateDocument(True)
+		End Sub
+	End Class
+End Namespace
