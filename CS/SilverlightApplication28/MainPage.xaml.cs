75c771686f5f96330597726a049979bd0322562e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication28/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Printing;
+using DevExpress.Xpf.Grid;
+
+namespace SilverlightApplication28 {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) {
+            PrintableControlLink link = new PrintableControlLink((TableView)gridControl1.View);
+            LinkPreviewModel model = new LinkPreviewModel(link);
+            documentPreview1.Model = model;
+            link.CreateDocument(true);
+        }
+    }
+}
---
+++ CS/SilverlightApplication28/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// ...
+
+namespace SilverlightApplication28 {
+    public class MainPageViewModel {
+        IEnumerable<Person> persons;
+
+        public IEnumerable<Person> Persons {
+            get {
+                return persons;
+            }
+            set {
+                persons = value;
+            }
+        }
+
+        public MainPageViewModel() {
+            persons = new List<Person>(new Person[] { 
+                new Person("John", 25),
+                new Person("Mary", 33)
+            });
+        }
+    }
+}
